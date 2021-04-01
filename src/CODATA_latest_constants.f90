27098b4ff5e95eb90ff848a5e4b15be66a93b2a5
vmagnin <vincent.magnin@libertysurf.fr>
2021-04-01 18:46:11 +0200

    New src/CODATA_latest_constants.f90 module pointing to the latest module

---
+++ src/CODATA_latest_constants.f90
@@ -0,0 +1,11 @@
+!-------------------------------------------------------------------------------
+! CODATA_latest_constants.f90
+! Updated 01-04-2021 with the 2018 values.
+! Next update should be in 2022.
+! Be aware that the names of a few constants may vary from one release to
+! another, and some constants may disappear or appear.
+!-------------------------------------------------------------------------------
+
+module codata_latest_constants
+  use codata_2018_constants
+end module codata_latest_constants
